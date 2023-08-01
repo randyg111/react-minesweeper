4de693f39b94c11662fcb1c083c1301d1506f5f3
randyg111 <ranboygt@gmail.com>
2023-08-01 15:19:21 -0500

    Typescript strict typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
 
-function Square({key, value, onSquareClick}) {
+type Grid = number[][];
+interface BoardProps {
+  squares: Grid;
+  onPlay: Function;
+}
+interface SquareProps {
+  value: number;
+  onSquareClick: React.MouseEventHandler;
+}
+
+function Square({value, onSquareClick}: SquareProps) {
   return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
-function Board({squares, onPlay}) {
-  function handleClick(r, c) {
+function Board({squares, onPlay}: BoardProps) {
+  function handleClick(r: number, c: number) {
     // if (calculateWin(squares)) {
     //   return;
     // }
-    const nextSquares = squares.map(arr => arr.slice());
+    const nextSquares = squares.map((arr: any[]) => arr.slice());
     nextSquares[r][c] = "x";
     onPlay(nextSquares);
   }
@@ -20,7 +30,7 @@ function Board({squares, onPlay}) {
   for (let r = 0; r < rows; r++) {
     const row = Array(cols);
     for (let c = 0; c < cols; c++) {
-        row[c] = <Square key={c} value={squares[r][c]} onSquareClick={() => handleClick(r, c)} />;
+        row[c] = <Square key={c} value={squares[r][c]} onSquareClick={() => handleClick(r, c)} />
     }
     // const row = squares[r].map((square, c) => {
     //   <Square key={c} value={c} onSquareClick={() => handleClick(r, c)} />
@@ -39,15 +49,15 @@ function Board({squares, onPlay}) {
 export default function Game() {
   const rows = 8;
   const cols = 8;
-  const [grid, setGrid] = useState(Array(rows).fill(Array(cols).fill(null)));
-  function handlePlay(nextGrid) {
+  const [grid, setGrid] = useState<Grid>(Array(rows).fill(Array(cols).fill(0)));
+  function handlePlay(nextGrid: Grid) {
     setGrid(nextGrid);
   }
 
   return (
     <div className="game">
       <div className="game-board">
-        <Board squares={grid} onPlay={handlePlay}/>
+        <Board squares={grid} onPlay={handlePlay} />
       </div>
       <div className="game-info">
         
