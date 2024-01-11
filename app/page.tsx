42cec9e37c792ff3beaf956d6152ea4d35eeac28
randyg111 <ranboygt@gmail.com>
2024-01-10 21:11:32 -0600

    Major fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 
-// optimize mine generation, win/lose calculation
-// convert name, github page, beat randy
-// minesweeper icon, theme
+// optimize mine generation, win/lose calculation, retry after win, timer start after mine revealed
+// github page
 type Value = "💥" | "🚩" | "⬜" | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | null;
 type Grid = Value[][];
 type GridFunc = (grid: Grid, r: number, c: number) => number;
@@ -121,13 +120,11 @@ function Board({squares, hiddenSquares, onPlay, flags, clicks, rightClick, check
     const boardRow = row.map((square, c) => {
       return <Square key={r * row.length + c} value={square} onSquareClick={(e: React.MouseEvent) => handleClick(e, r, c)} />
     });
-    return <div key={r} className="h-8">{boardRow}</div>
+    return <div key={r} className="flex">{boardRow}</div>
   })
 
-  
-
   return (
-    <div>
+    <div className={"h-" + (8*board.length)}>
       {board}
     </div>
   );
@@ -146,11 +143,11 @@ export default function Game() {
   const [rightClick, setRightClick] = useState(false);
   const [safeStart, setSafeStart] = useState(false);
   const [startedSafe, setStartedSafe] = useState(false);
-  const [isClient, setIsClient] = useState(false)
+  const [isClient, setIsClient] = useState(false);
  
   useEffect(() => {
     setIsClient(true)
-  }, [])
+  }, []);
 
   const createHidden = useCallback(() => {
     const temp = Array(rows).fill(null).map(x => Array(cols).fill(0)) as Grid;
@@ -298,7 +295,7 @@ export default function Game() {
     const safeDisplay = safeStart ? " with Safe Start" : "";
     const vals = Object.values(Difficulty);
     const times = vals.map((val, i) => {
-      return <p key={i}>Best {val} time{safeDisplay}: {getLocalStorage("best"+val+"Time"+safe)}</p>
+      return <div key={i}>Best {val} time{safeDisplay}: {getLocalStorage("best"+val+"Time"+safe)}</div>
     });
     return (
       <>
@@ -325,33 +322,44 @@ export default function Game() {
     if (isClient) {
       return localStorage.getItem(k);
     }
+    return null;
   }
 
   function setLocalStorage(k: string, v: string) {
     if (isClient) {
-      return localStorage.setItem(k, v);
+      localStorage.setItem(k, v);
     }
   }
 
   return (
-    <div className="game">
-      <div className="game-display">
-        <div className="status">{counter}</div>
-        <div className="game-board" onMouseLeave={handleMouseLeave}>
-          <Board squares={grid} onPlay={handlePlay} hiddenSquares={hidden} flags={flags} clicks={clicks} rightClick={rightClick} checkSafeStart={checkSafeStart} />
-        </div>
-      </div>
-      <div className="game-info">
+    <div className="grid grid-rows-8 justify-center">
+      <div className="flex justify-evenly">
+        <div>{counter}</div>
         <div>{formatTime(time)}</div>
+      </div>
+      <div className="flex justify-center" onMouseLeave={handleMouseLeave}>
+        <Board squares={grid} onPlay={handlePlay} hiddenSquares={hidden} flags={flags} clicks={clicks} rightClick={rightClick} checkSafeStart={checkSafeStart} />
+      </div>
+      <div className="flex justify-evenly">
         <button onClick={handleBeginnerClick}>{Difficulty.beginner}</button>
         <button onClick={handleIntermediateClick}>{Difficulty.intermediate}</button>
         <button onClick={handleExpertClick}>{Difficulty.expert}</button>
+      </div>
+      <div className="grid grid-rows-4 justify-center">
         <CustomForm onSubmit={handleSubmit} rows={rows} cols={cols} mines={mines} />
+      </div>
+      <div className="flex justify-center">
         <button onClick={retry}>Retry</button>
-        <p>{status}</p>
+      </div>
+      <div className="flex justify-center">
+        {status}
+      </div>
+      <div className="flex justify-evenly">
         {bestTimes}
+      </div>
+      <div className="flex justify-center">
         <label>
-          Safe Start: <input checked={safeStart} onChange={() => {setSafeStart(!safeStart)}}type="checkbox" />
+          Safe Start: <input checked={safeStart} onChange={() => {setSafeStart(!safeStart); retry();}} type="checkbox" />
         </label>
       </div>
     </div>
@@ -372,21 +380,18 @@ function CustomForm({onSubmit, rows, cols, mines}: FormProps) {
     setNextMines(mines);
   }
   return (
-    <div>
+    <>
       <label>
         Number of rows: <input value={nextRows} onChange={e => setNextRows(Number(e.target.value))} type="number" />
       </label>
-      <br />
       <label>
         Number of columns: <input value={nextCols} onChange={e => setNextCols(Number(e.target.value))} type="number" />
       </label>
-      <br />
       <label>
         Number of mines: <input value={nextMines} onChange={e => setNextMines(Number(e.target.value))} type="number" />
       </label>
-      <br />
       <button onClick={() => onSubmit(nextRows, nextCols, nextMines)}>Submit</button>
-    </div>
+    </>
   );
 }
 
@@ -441,117 +446,3 @@ function colFunc(grid: Grid, r: number, c: number, func: GridFunc) {
   }
   return num;
 }
-
-// import Image from 'next/image'
-
-// export default function Home() {
-//   return (
-//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-//       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-//         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-//           Get started by editing&nbsp;
-//           <code className="font-mono font-bold">app/page.tsx</code>
-//         </p>
-//         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-//           <a
-//             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-//             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             By{' '}
-//             <Image
-//               src="/vercel.svg"
-//               alt="Vercel Logo"
-//               className="dark:invert"
-//               width={100}
-//               height={24}
-//               priority
-//             />
-//           </a>
-//         </div>
-//       </div>
-
-//       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-//         <Image
-//           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-//           src="/next.svg"
-//           alt="Next.js Logo"
-//           width={180}
-//           height={37}
-//           priority
-//         />
-//       </div>
-
-//       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-//         <a
-//           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2 className={`mb-3 text-2xl font-semibold`}>
-//             Docs{' '}
-//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-//               -&gt;
-//             </span>
-//           </h2>
-//           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-//             Find in-depth information about Next.js features and API.
-//           </p>
-//         </a>
-
-//         <a
-//           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-//           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2 className={`mb-3 text-2xl font-semibold`}>
-//             Learn{' '}
-//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-//               -&gt;
-//             </span>
-//           </h2>
-//           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-//             Learn about Next.js in an interactive course with&nbsp;quizzes!
-//           </p>
-//         </a>
-
-//         <a
-//           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2 className={`mb-3 text-2xl font-semibold`}>
-//             Templates{' '}
-//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-//               -&gt;
-//             </span>
-//           </h2>
-//           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-//             Explore the Next.js 13 playground.
-//           </p>
-//         </a>
-
-//         <a
-//           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2 className={`mb-3 text-2xl font-semibold`}>
-//             Deploy{' '}
-//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-//               -&gt;
-//             </span>
-//           </h2>
-//           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-//             Instantly deploy your Next.js site to a shareable URL with Vercel.
-//           </p>
-//         </a>
-//       </div>
-//     </main>
-//   )
-// }
